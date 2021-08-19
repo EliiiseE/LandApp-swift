868da541a5ae322cf45acdddf56ef6ed6c8910bb
EliiiseE <elise.echasseriau@mmibordeaux.com>
2021-08-19 10:53:22 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Elise Echasseriau on 19/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
